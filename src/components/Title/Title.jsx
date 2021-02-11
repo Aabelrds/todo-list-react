61985ab42766035d6c30c94c1f8b11ff994940c9
Juan <info@juanmacias.es>
2021-02-11 22:08:59 +0100

    add div hola

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -9,6 +9,7 @@ class Title extends Component {
       <section className="title">
         <div className="title__left">
             <h3>{this.props.title || ''}</h3>
+            <div>Hola</div>
         </div>
         <div className="title__right">
             <FontAwesomeIcon className="title__right__item" icon={faCommentAlt} />
